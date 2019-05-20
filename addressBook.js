5d01bce975cc1df2a0622d9290a3a34ea8ee0208
Kelly T Nguyen <Kelly.Nguyen@medspring.local>
2019-05-19 20:55:43 -0500

    finished address book:

--- addressBook.js
+++ addressBook.js
@@ -1,19 +1,65 @@
-function getUsers(){
-    fetch("https://randomuser.me/api/")
-    .then((res) => res.json())
-    .then((data) => { 
-        let output = '<h2> User </h2'>
-        data.(function(){
-            output += `
-            <div>
-                <span>Name: ${results["0"].name.first} </span>
-                <img src="${} </img>
-            </div>
-            `
+window.onload = function() {
+    allUsers();
+  };
+
+const newArray = [];
+function get(){
+
+    fetch('https://randomuser.me/api/')
+      .then( response => response.json())
+        .then(data => {
+            newArray.push(data.results["0"])
+            console.log(newArray);
         })
-    document.getElementById('output').innerHTML = output;
-    })
+  
+        document.getElementById("contacts").innerHTML = " ";
+
+        // list out users by name and picture
+        newArray.map(person => {
+            console.log(person);
+            let createLi = document.createElement("li");
+            let contactList = document.getElementById("contacts");
+            let image = document.createElement("img");
+            image.src = person.picture.thumbnail;
+            createLi.appendChild(image);
+            createLi.appendChild(document.createTextNode(person.name.first + " " + person.name.last));
+            contactList.append(createLi);
+        });
 }
 
-    // .catch( err => console.log("Oops, looks like we got an error: ", err))
-    // .finally( ()=> console.log("Finally, This function always runs.."))
+// getting multiple users in one array
+function allUsers() {
+    let multipleArray = null;
+    fetch('https://randomuser.me/api/?results=10')
+    .then (response => response.json())
+    .then (data => {
+        multipleArray = data.results
+        multipleArray.map(person => {
+        console.log(person);
+        let createAllLi = document.createElement("li");
+        let allContactsList = document.getElementById("allContacts");
+        let allImage = document.createElement("img");
+        
+// this button will give you more information about the contact
+        let button = document.createElement('button');
+        button.addEventListener("click",(e) => {
+            let textBox = document.createElement('p');
+            let pText = document.createTextNode("Cell: " + person.cell + " " + "Age: " + person.dob.age);
+            textBox.appendChild(pText);
+            createAllLi.appendChild(textBox);
+        })
+        allImage.src = person.picture.thumbnail;
+        createAllLi.appendChild(allImage);
+        createAllLi.appendChild(document.createTextNode(person.name.first + " " + person.name.last));
+        createAllLi.appendChild(button);
+
+        let buttonText = document.createTextNode("More Info");
+  
+        button.appendChild(buttonText);
+        allContactsList.append(createAllLi);
+    })
+      console.log(multipleArray);
+    })
+
+    document.getElementById("allContacts").innerHTML = " ";
+}
